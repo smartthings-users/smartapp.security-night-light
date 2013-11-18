52aa638585ed6ef46027f568b9bdf8967fd2035f
Brian Steere <dianoga7@3dgo.net>
2013-11-18 16:35:57 -0600

    Update security-night-light.smartapp.groovy

--- security-night-light.smartapp.groovy
+++ security-night-light.smartapp.groovy
@@ -2,23 +2,6 @@
  *  Clever Night Light
  *
  *  Author: Brian Steere
- *  Code: https://github.com/smartthings-users/smartapp.security-night-light
- *
- * Copyright (C) 2013 Brian Steere <dianoga7@3dgo.net>
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
- * software and associated documentation files (the "Software"), to deal in the Software 
- * without restriction, including without limitation the rights to use, copy, modify, 
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
- * permit persons to whom the Software is furnished to do so, subject to the following 
- * conditions: The above copyright notice and this permission notice shall be included 
- * in all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
  preferences {
@@ -26,10 +9,10 @@
     section("Turn on which things") {
         input "switches", "capability.switch", title: "Things", multiple: true
         input "allowOverride", "enum", title: "Allow manual override?", multiple: false, metadata: [
-            values: [
-                'Yes',
-                'No'
-            ]
+        values: [
+        'Yes',
+        'No'
+        ]
         ]
     }
 
@@ -91,12 +74,9 @@ def changedLocationMode(evt) {
     log.debug "Mode: ${location.currentMode}"
     if(location.currentMode == startMode) {
         modeStartThings()
-    }
-    else if(state.modeStarted) {
+    } else {
         modeStopThings()
-    }
-    else if(state.motionStarted) {
-        motionStopThings()
+        state.motionStarted = false
     }
 }
 
